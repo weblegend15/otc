d676a3eb81cc0cd3be34f5fbdd938310afe869c3
weblegend15 <warrenanderson4036@gmail.com>
2019-03-18 16:21:34 -0500

    feat(app): configure redux-form

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
+import { reducer as reduxFormReducer } from 'redux-form';
 
 import dataReducer from './data';
 
 export default combineReducers({
   routing: routerReducer,
+  form: reduxFormReducer,
   userData: dataReducer,
 });
