5c6eb96f8f2dd4817dec90e488938d2bacdc49fd
weblegend15 <warrenanderson4036@gmail.com>
2019-03-18 19:03:35 -0500

    feat(Topbar): proper topbar based on auth

--- src/layouts/Topbar/Topbar.js
+++ src/layouts/Topbar/Topbar.js
@@ -1,28 +1,82 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button';
 import Nav from 'react-bootstrap/Nav';
 import { NavLink } from 'react-router-dom';
+import { history } from '../../configureStore';
 import logoIcon from '../../assets/icons/logo.svg';
 
-export default () => (
-  <Navbar sticky="top" bg="dark" variant="dark" expand="md">
-    <Navbar.Brand href="#">
-      <img className="d-inline-block align-top" src={logoIcon} alt="logo" /> {'  T  R  A  D  E'}
-    </Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto">
-        <NavLink to="/home">
-          Home
-        </NavLink>
-        <NavLink to="/dashboard">
-          Dashboard
-        </NavLink>
-      </Nav>
-      <Nav>
-        <Button variant="primary">Profile</Button>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-);
+import Button from '../../components/Buttons/Button';
+
+class Topbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  handleLogout = () => {
+    const { signout } = this.props;
+    signout();
+    history.push('/signin');
+  }
+
+  handleSingin = () => {
+    history.push('/signin');
+  }
+
+  renderAuthNav = () => {
+    return (
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <NavLink to="/home">
+            Home
+          </NavLink>
+          <NavLink to="/dashboard">
+            Dashboard
+          </NavLink>
+        </Nav>
+        <Nav>
+          <Button variant="primary" onClick={this.handleLogout}>Log out</Button>
+        </Nav>
+      </Navbar.Collapse>
+    );
+  }
+
+  renderNoAuthNav = () => {
+    return (
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <NavLink to="/home">
+            Home
+          </NavLink>
+          <NavLink to="/faq">
+            FAQ
+          </NavLink>
+          <NavLink to="/contactus">
+            Contact Us
+          </NavLink>
+        </Nav>
+        <Nav>
+          <Button variant="outline-primary" onClick={this.handleSingin}>SIGN IN</Button>
+        </Nav>
+      </Navbar.Collapse>
+    );
+  }
+
+  render() {
+    const { currentUser } = this.props;
+
+    return (
+      <Navbar sticky="top" bg="dark" variant="dark" expand="md">
+        <Navbar.Brand href="#">
+          <img className="d-inline-block align-top" src={logoIcon} alt="logo" /> {'  T  R  A  D  E'}
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        {!currentUser.isAuthenticated ? this.renderNoAuthNav() : this.renderAuthNav()}
+      </Navbar>
+    );
+  }
+}
+
+export default Topbar;
--- src/layouts/Topbar/index.js
+++ src/layouts/Topbar/index.js
@@ -1,4 +1,31 @@
-import Navbar from './Topbar';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Topbar from './Topbar';
+
 import './styles.scss';
 
-export default Navbar;
+import { signout } from '../../screens/Signin/redux/actions';
+
+Topbar.propTypes = {
+  currentUser: PropTypes.objectOf(PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+    PropTypes.object,
+  ])).isRequired,
+  signout: PropTypes.func,
+};
+
+Topbar.defaultTypes = {
+  currentUser: {},
+  signout: null,
+};
+
+const mapStateToProps = (state) => ({
+
+});
+
+const mapDispatchToProps = {
+  signout,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Topbar);
