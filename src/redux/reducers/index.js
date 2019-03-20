6d7dc8c07c51b32fdc2feae20c69d6086e178c14
weblegend15 <warrenanderson4036@gmail.com>
2019-03-20 10:29:23 -0500

    refactor(redux): remove unnecessary redux config

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,12 +2,10 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import { reducer as reduxFormReducer } from 'redux-form';
 
-import dataReducer from './data';
 import signinReducer from '../../screens/Signin/redux/reducers';
 
 export default combineReducers({
   routing: routerReducer,
   form: reduxFormReducer,
-  userData: dataReducer,
   signin: signinReducer,
 });
