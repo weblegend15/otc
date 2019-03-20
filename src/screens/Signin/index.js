74c4ca999cbc898189707b131eb73cdf0044ea3e
weblegend15 <warrenanderson4036@gmail.com>
2019-03-20 10:36:07 -0500

    feat(Signin): redux form validation and connect signin endpoint

--- src/screens/Signin/index.js
+++ src/screens/Signin/index.js
@@ -1,23 +1,38 @@
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-
+import { reduxForm } from 'redux-form';
+import validate from './validate';
 import Signin from './Signin';
 import { signinRequest } from './redux/actions';
 
 Signin.propTypes = {
   signinRequest: PropTypes.func,
+  signinFormState: PropTypes.objectOf(PropTypes.object),
+  currentUser: PropTypes.objectOf([
+    PropTypes.object,
+    PropTypes.bool,
+  ]),
 };
 
 Signin.defaultProps = {
-  signinRequest: null,
+  signinRequest: () => {},
+  signinFormState: {},
+  currentUser: {},
 };
 
 const mapStateToProps = (state) => ({
-
+  signinFormState: state.form.signinForm,
+  currentUser: state.signin.currentUser,
 });
 
 const mapDispatchToProps = {
   signinRequest,
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Signin);
+const withReduxform = reduxForm({
+  form: 'signinForm',
+  validate,
+  asyncBlurFields: ['email'],
+})(Signin);
+
+export default connect(mapStateToProps, mapDispatchToProps)(withReduxform);
--- src/screens/Signin/redux/reducers.js
+++ src/screens/Signin/redux/reducers.js
@@ -2,36 +2,52 @@ import {
   SIGNIN_ERROR,
   SIGNIN_SUCCESS,
   SIGNOUT,
+  SIGNIN_REQUEST,
 } from './constants';
 
 const initialState = {
-  authenticated: false,
-  userData: {},
+  currentUser: {
+    value: null,
+    loading: false,
+  },
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case SIGNIN_REQUEST:
+      return {
+        ...state,
+        currentUser: {
+          ...state.currentUser,
+          loading: true,
+        },
+      };
+
     case SIGNIN_SUCCESS:
       localStorage.setItem('token', action.payload.token);
       return {
         ...state,
-        authenticated: true,
-        userData: action.payload,
+        currentUser: {
+          value: action.payload.user,
+          loading: false,
+        },
       };
 
     case SIGNIN_ERROR:
       localStorage.removeItem('token');
       return {
         ...state,
-        authenticated: false,
+        currentUser: {
+          ...state.currentUser,
+          loading: false,
+        },
       };
 
     case SIGNOUT:
       localStorage.removeItem('token');
       return {
         ...state,
-        userData: {},
-        authenticated: false,
+        currentUser: {},
       };
 
     default:
