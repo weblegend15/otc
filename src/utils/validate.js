db0eed53e00b343acc7ad2ee817188534c7f5056
weblegend15 <warrenanderson4036@gmail.com>
2019-03-20 12:36:02 -0500

    feat(app): define validate functions

---
+++ src/utils/validate.js
@@ -0,0 +1,13 @@
+export const required = value => value ? undefined : 'Required';
+
+export const maxLength = max => value =>
+  value && value.length > max ? `Must be ${max} characters or less` : undefined;
+
+export const minLength = value =>
+  value && value.length < 6 ? 'Must be 6 characters or more' : undefined;
+
+export const isNumber = value => value && isNaN(Number(value)) ? 'Must be a number' : undefined; // eslint-disable-line
+
+export const isEmail = value =>
+  value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ?
+    'Invalid email address' : undefined;
