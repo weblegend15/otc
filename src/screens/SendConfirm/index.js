c5bc13a470abba016d168f0fecf2c6b6a398c90e
weblegend15 <warrenanderson4036@gmail.com>
2019-03-21 18:54:29 -0500

    feat(screens): create SendConfirm screen

---
+++ src/screens/SendConfirm/index.js
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import SendConfirm from './SendConfirm';
+
+SendConfirm.propTypes = {
+  email: PropTypes.string,
+};
+
+SendConfirm.defaultProps = {
+  email: '',
+};
+
+const mapStateToProps = state => ({
+  email: state.confirm.email,
+});
+
+export default connect(mapStateToProps)(SendConfirm);
