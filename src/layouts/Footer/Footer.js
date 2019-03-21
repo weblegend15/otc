cb41ad7a07c06983e5ee6542cae70325ac833f84
weblegend15 <warrenanderson4036@gmail.com>
2019-03-21 18:51:30 -0500

    update: Footer

--- src/layouts/Footer/Footer.js
+++ src/layouts/Footer/Footer.js
@@ -5,7 +5,13 @@ import { NavLink } from 'react-router-dom';
 
 export default () => {
   return (
-    <Navbar className="d-flex footer flex-column" sticky="top" bg="secondary" variant="secondary" expand="md">
+    <Navbar
+      className="d-flex footer flex-column"
+      sticky="top"
+      bg="secondary"
+      variant="secondary"
+      expand="md"
+    >
       <Nav className="mx-auto">
         <NavLink className="mr-5 ml-5 mt-3 mb-3" to="/home">
           Home
@@ -23,7 +29,9 @@ export default () => {
           Privacy Policy
         </NavLink>
       </Nav>
-      <p className="mt-3">Copyright © 2019 <b>OTC trade.</b> All rights reserved.</p>
+      <p className="mt-3">
+        Copyright © 2019 <b>OTC trade.</b> All rights reserved.
+      </p>
     </Navbar>
   );
 };
