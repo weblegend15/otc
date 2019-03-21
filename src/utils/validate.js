99d31d89438be07c20e963d39a24143eafe611a1
weblegend15 <warrenanderson4036@gmail.com>
2019-03-21 18:50:11 -0500

    chore(app): add validate and normalize functions

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,4 +1,4 @@
-export const required = value => value ? undefined : 'Required';
+export const required = value => (value ? undefined : 'Required');
 
 export const maxLength = max => value =>
   value && value.length > max ? `Must be ${max} characters or less` : undefined;
@@ -6,8 +6,14 @@ export const maxLength = max => value =>
 export const minLength = value =>
   value && value.length < 6 ? 'Must be 6 characters or more' : undefined;
 
-export const isNumber = value => value && isNaN(Number(value)) ? 'Must be a number' : undefined; // eslint-disable-line
+export const isNumber = value =>
+  value && isNaN(Number(value)) ? 'Must be a number' : undefined; // eslint-disable-line
 
 export const isEmail = value =>
-  value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ?
-    'Invalid email address' : undefined;
+  value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
+    ? 'Invalid email address'
+    : undefined;
+
+export const confirmation = (value, { stepOne: { password } }) => {
+  return value && value !== password ? 'Password mismatched' : undefined;
+};
