843d6cec75b06a169406540dba2880ab878e8853
weblegend15 <warrenanderson4036@gmail.com>
2019-03-21 19:03:23 -0500

    refactor(components): clean up Card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,14 +1,3 @@
-import React from 'react';
-import cx from 'classnames';
 import Card from 'react-bootstrap/Card';
 
-export default (props) => {
-  const { children, className } = props;
-  const customClass = cx({ [className]: !!className });
-
-  return (
-    <Card {...props} className={customClass} >
-      {children}
-    </Card>
-  );
-};
+export default Card;
