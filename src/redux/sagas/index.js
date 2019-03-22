b8e8a5194b25b63b8d75cbdaec9384794af7dd31
weblegend15 <warrenanderson4036@gmail.com>
2019-03-21 19:31:22 -0500

    fix(app): resolve saga import issue

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -2,14 +2,14 @@ import { all, fork } from 'redux-saga/effects';
 
 import signinSaga from '../../screens/Signin/redux/saga';
 import signupSaga from '../../screens/Signup/redux/saga';
-import confirmSaga from '../../screens/Confirm/redux/saga';
+import sendConfirmSaga from '../../screens/SendConfirm/redux/saga';
 import verifyEmailSaga from '../../screens/VerifyEmail/redux/saga';
 
 export default function* root() {
   yield all([
     fork(signinSaga),
     fork(signupSaga),
-    fork(confirmSaga),
+    fork(sendConfirmSaga),
     fork(verifyEmailSaga),
   ]);
 }
