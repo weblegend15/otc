fbc18396a33dd756a9ac9519286b3b2842fe4c13
weblegend15 <warrenanderson4036@gmail.com>
2019-03-21 19:03:57 -0500

    chore(app): routes config

--- src/routes.js
+++ src/routes.js
@@ -3,43 +3,57 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Route, withRouter, Switch, Redirect } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.min.css'; 
+import 'react-toastify/dist/ReactToastify.min.css';
 import { Topbar, App, Dashboard, Footer } from './layouts';
 
-import { Home, Signin, Signup, FAQ, ContactUs } from './screens';
+import {
+  Signin,
+  Signup,
+  FAQ,
+  ContactUs,
+  Terms,
+  SendConfirm,
+  VerifyEmail,
+} from './screens';
 
 class MainRoutes extends Component {
-  renderAuthRoutes = () => {
-    return (
-      <Switch>
-        <Route exact path="/home" component={App} />
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Redirect to="/home" />
-      </Switch>
-    );
-  }
+  renderAuthRoutes = () => (
+    <Switch>
+      <Route exact path="/faq" component={FAQ} />
+      <Route exact path="/terms" component={Terms} />
+      <Route exact path="/contactus" component={ContactUs} />
 
-  renderNoAuthRoutes = () => {
-    return (
-      <Switch>
-        <Route exact path="/signin" component={Signin} />
-        <Route exact path="/signup" component={Signup} />
-        <Route exact path="/faq" component={FAQ} />
-        <Route exact path="/contactus" component={ContactUs} />
-        <Redirect to="/signin" />
-      </Switch>
-    );
-  }
+      <Route exact path="/home" component={App} />
+      <Route exact path="/dashboard" component={Dashboard} />
+      <Redirect to="/home" />
+    </Switch>
+  );
+
+  renderNoAuthRoutes = () => (
+    <Switch>
+      <Route exact path="/faq" component={FAQ} />
+      <Route exact path="/terms" component={Terms} />
+      <Route exact path="/contactus" component={ContactUs} />
+
+      <Route exact path="/signin" component={Signin} />
+      <Route exact path="/signup" component={Signup} />
+      <Route exact path="/send-confirm" component={SendConfirm} />
+      <Route exact path="/auth/verify-email" component={VerifyEmail} />
+      <Redirect to="/signin" />
+    </Switch>
+  );
 
   render() {
     const { currentUser } = this.props;
 
     return (
       <Fragment>
+        <Topbar currentUser={currentUser} />
+        <ToastContainer pauseOnFocusLoss={false} autoClose={3000} />
         <div className="content">
-          <Topbar currentUser={currentUser} />
-          <ToastContainer pauseOnFocusLoss={false} autoClose={3000} />
-          {!currentUser.value ? this.renderNoAuthRoutes() : this.renderAuthRoutes()}
+          {!currentUser.value
+            ? this.renderNoAuthRoutes()
+            : this.renderAuthRoutes()}
         </div>
         <Footer />
       </Fragment>
@@ -55,7 +69,7 @@ MainRoutes.defaultProps = {
   currentUser: {},
 };
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   currentUser: state.signin.currentUser,
 });
 
