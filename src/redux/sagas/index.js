dac532d31dbae4f6d0fed22d20b6c491e4ced598
weblegend15 <warrenanderson4036@gmail.com>
2019-03-21 19:03:38 -0500

    chore(app): redux config

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,9 +1,15 @@
 import { all, fork } from 'redux-saga/effects';
 
 import signinSaga from '../../screens/Signin/redux/saga';
+import signupSaga from '../../screens/Signup/redux/saga';
+import confirmSaga from '../../screens/Confirm/redux/saga';
+import verifyEmailSaga from '../../screens/VerifyEmail/redux/saga';
 
 export default function* root() {
   yield all([
     fork(signinSaga),
+    fork(signupSaga),
+    fork(confirmSaga),
+    fork(verifyEmailSaga),
   ]);
 }
