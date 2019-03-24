905f3c51647d2e9bfd565c355923e85d66579069
weblegend15 <warrenanderson4036@gmail.com>
2019-03-24 07:06:13 -0500

    update: layouts

--- src/layouts/App/index.js
+++ src/layouts/App/index.js
@@ -1,19 +1,3 @@
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-
 import App from './App';
-import './App.scss';
 
-App.propTypes = {
-
-};
-
-const mapStateToProps = (state) => ({
-
-});
-
-const mapDispatchToProps = {
-
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
