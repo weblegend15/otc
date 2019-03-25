4a75cb9aae96fc10df523086285f83eccdaceef9
weblegend15 <warrenanderson4036@gmail.com>
2019-03-25 11:33:22 -0500

    feat(screens): add profile & setting routes

--- src/screens/app/index.js
+++ src/screens/app/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 import { Route, withRouter, Switch } from 'react-router-dom';
 
-import Profile from './Profile';
+import UserProfile from './UserProfile';
+import UserSetting from './UserSetting';
 import Users from './Users';
 import MyGroups from './MyGroups';
 
 const App = ({ match }) => (
   <Switch>
-    <Route path={`${match.url}/profile`} component={Profile} />
+    <Route path={`${match.url}/profile`} component={UserProfile} />
+    <Route path={`${match.url}/setting`} component={UserSetting} />
     <Route path={`${match.url}/users`} component={Users} />
     <Route path={`${match.url}/my-groups`} component={MyGroups} />
   </Switch>
