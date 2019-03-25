fbe80e46440a7491d9ee82a682485ada4a4ab350
weblegend15 <warrenanderson4036@gmail.com>
2019-03-25 11:30:05 -0500

    update(components): clean up Button

--- src/components/Button/Button/Button.js
+++ src/components/Button/Button/Button.js
@@ -1,6 +1,1 @@
-import React from 'react';
-import BootstrapButton from 'react-bootstrap/Button';
-
-export default ({ children, ...props }) => (
-  <BootstrapButton {...props}>{children}</BootstrapButton>
-);
+export { default } from 'react-bootstrap/Button';
