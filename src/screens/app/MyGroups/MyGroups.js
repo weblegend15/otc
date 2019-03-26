1d65256762c2a9e1f8b6791b6770f612a89750a9
weblegend15 <warrenanderson4036@gmail.com>
2019-03-25 21:24:01 -0500

    update(app): app home page

--- src/screens/app/MyGroups/MyGroups.js
+++ src/screens/app/MyGroups/MyGroups.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import { Card } from '../../../components';
 
-export default () => <Card className="m-5 p-5">My Groups page</Card>;
+export default () => <Card className="p-5">My Groups page</Card>;
