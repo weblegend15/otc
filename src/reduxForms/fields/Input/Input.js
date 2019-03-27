9cade7a41f2a737a15aabe9cc3a71ea875aba9f4
weblegend15 <warrenanderson4036@gmail.com>
2019-03-27 14:35:03 -0500

    feat(reduxForm): create form for new group modal

--- src/reduxForms/fields/Input/Input.js
+++ src/reduxForms/fields/Input/Input.js
@@ -8,12 +8,16 @@ export default ({
   type,
   placeholder,
   meta: { touched, error },
+  as,
+  rows,
   ...rest
 }) => (
   <FieldWrapper {...rest} error={error} touched={touched} label={label}>
     <Form.Control
       {...input}
       type={type}
+      as={as}
+      rows={rows}
       placeholder={placeholder}
       isInvalid={touched && error}
       autoComplete="new-password"
