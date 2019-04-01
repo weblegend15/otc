01fca71ab4bf51cc73605bc7817d80aea90a91df
weblegend15 <warrenanderson4036@gmail.com>
2019-04-01 05:28:56 -0500

    update: add link on group title to group profile

--- src/screens/app/Groups/GroupsList/GroupCard/GroupCard.js
+++ src/screens/app/Groups/GroupsList/GroupCard/GroupCard.js
@@ -6,7 +6,9 @@ export default ({ name, status, description, groupId }) => (
   <Card className="group-card">
     <Card.Body>
       <Card.Title className="d-flex flex-row justify-content-between align-items-center">
-        <h4 className="font-weight-bold">{name}</h4>
+        <Link to={`/app/groups/${groupId}`}>
+          <h4 className="font-weight-bold">{name}</h4>
+        </Link>
         <div className="group-card-status text-dark d-flex flex-row">
           <Icon name="lock" className="mr-3" />
           <p className="m-0 p-md d-flex align-items-center">{status}</p>
