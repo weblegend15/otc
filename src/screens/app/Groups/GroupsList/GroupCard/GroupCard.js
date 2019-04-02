5f2982110a61d01af5b2169cb0e20de7b979e179
weblegend15 <warrenanderson4036@gmail.com>
2019-04-02 05:41:24 -0500

    fix(app): add delete permission for group admin

--- src/screens/app/Groups/GroupsList/GroupCard/GroupCard.js
+++ src/screens/app/Groups/GroupsList/GroupCard/GroupCard.js
@@ -1,6 +1,8 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { Card, Icon, Button } from '../../../../../components';
+
+import { checkGroupPermission } from '../../../../../utils/permission';
 
 export default ({
   name,
@@ -10,6 +12,8 @@ export default ({
   currentUser,
   toggleModal,
 }) => {
+  const { isGroupAdmin } = checkGroupPermission(currentUser, groupId);
+
   return (
     <Card className="group-card">
       <Card.Body>
@@ -17,30 +21,32 @@ export default ({
           <Link to={`/app/groups/${groupId}`}>
             <h4 className="font-weight-bold">{name}</h4>
           </Link>
-          <div className="group-card-status text-dark d-flex flex-row">
+          <div className="group-card-status text-dark d-flex flex-row font-weight-semibold">
             <Icon name="lock" className="mr-3" />
-            <p className="m-0 p-md d-flex align-items-center">{status}</p>
+            <p className="m-0 p-md d-flex align-items-center p-sm">{status}</p>
           </div>
         </Card.Title>
         <Card.Text className="text-dark mb-4">{description}</Card.Text>
       </Card.Body>
       <Card.Footer className="py-4 border-0 d-flex flex-row justify-content-between align-items-center">
-        {currentUser.role === 'ADMIN' && (
-          <Fragment>
-            <Button
-              variant="outline-link"
-              onClick={() => toggleModal('deleteGroupModal', groupId)}
-            >
-              <Icon name="trash" className="h3-title opacity-5" />
-            </Button>
-            <Button
-              variant="outline-link"
-              onClick={() => toggleModal('approveGroupModal', groupId)}
-            >
-              <Icon name="thumbs-up" className="h3-title opacity-5" />
-            </Button>
-          </Fragment>
+        {(currentUser.role === 'ADMIN' || isGroupAdmin) && (
+          <Button
+            variant="outline-link"
+            onClick={() => toggleModal('deleteGroupModal', groupId)}
+          >
+            <Icon name="trash" className="h3-title opacity-5" />
+          </Button>
         )}
+
+        {currentUser.role === 'ADMIN' && (
+          <Button
+            variant="outline-link"
+            onClick={() => toggleModal('approveGroupModal', groupId)}
+          >
+            <Icon name="thumbs-up" className="h3-title opacity-5" />
+          </Button>
+        )}
+
         <Link
           to={`/app/groups/${groupId}`}
           className="btn btn-primary ml-auto font-weight-bold"
