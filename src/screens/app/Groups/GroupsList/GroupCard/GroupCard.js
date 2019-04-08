d731cd43fa037a1140e4e568e6dc7f21067765b0
weblegend15 <warrenanderson4036@gmail.com>
2019-04-08 17:59:26 -0500

    chore(styles): group list card

--- src/screens/app/Groups/GroupsList/GroupCard/GroupCard.js
+++ src/screens/app/Groups/GroupsList/GroupCard/GroupCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Truncate from 'react-truncate';
 import { Card, Icon, Button } from '../../../../../components';
 
 import { checkGroupPermission } from '../../../../../utils/permission';
@@ -19,14 +20,14 @@ export default ({
 
   return (
     <Card className="group-card">
-      <Card.Body>
+      <Card.Body className="p-4">
         <Card.Title className="d-flex flex-row justify-content-between align-items-center">
           <Link
             to={`/app/groups/${groupId}${
               currentUser.role === 'ADMIN' || isGroupAdmin ? '/admin' : ''
             }`}
           >
-            <h4 className="font-weight-bold">{name}</h4>
+            <h4 className="font-weight-bold line-ellipsis">{name}</h4>
           </Link>
 
           <div className="group-card-status text-dark d-flex flex-row font-weight-semibold">
@@ -34,11 +35,14 @@ export default ({
             <p className="m-0 p-md d-flex align-items-center p-sm">{status}</p>
           </div>
         </Card.Title>
-        <Card.Text className="text-dark mb-4">{description}</Card.Text>
+        <Card.Text className="text-dark mb-2">
+          <Truncate lines={2}>{description}</Truncate>
+        </Card.Text>
       </Card.Body>
-      <Card.Footer className="py-4 border-0 d-flex flex-row justify-content-between align-items-center">
+      <Card.Footer className="py-3 px-4 border-0 d-flex flex-row justify-content-between align-items-center">
         {(currentUser.role === 'ADMIN' || isGroupAdmin) && (
           <Button
+            className="mr-4"
             variant="outline-link"
             onClick={() => toggleModal('deleteGroupModal', groupId)}
           >
@@ -59,6 +63,7 @@ export default ({
           {isApplied && 'Applied'}
           {isBanned && 'Banned'}
           {isMember && 'Member'}
+          {isGroupAdmin && 'Group Admin'}
         </div>
 
         {!isGroupAdmin && !isBanned && !isMember && !isApplied && (
