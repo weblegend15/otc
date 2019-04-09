490944a3e2ea4a541bc0217f604a9fb7cfdd546c
weblegend15 <warrenanderson4036@gmail.com>
2019-04-09 03:21:41 -0500

    feat(reduxForm): create ProposalForm

--- src/reduxForms/index.js
+++ src/reduxForms/index.js
@@ -2,10 +2,12 @@ import JoinGroupModalForm from './JoinGroupModalForm';
 import NewGroupModalForm from './NewGroupModalForm';
 import NewOfferModalForm from './NewOfferModalForm';
 import EditGroupForm from './EditGroupForm';
+import ProposalForm from './ProposalForm';
 
 export {
   JoinGroupModalForm,
   NewGroupModalForm,
   NewOfferModalForm,
   EditGroupForm,
+  ProposalForm,
 };
