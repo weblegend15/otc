0a52b938bdc45eaf24b4fb4f8b6360bb119a7ab9
dblash15 <dmitryblash@gmail.com>
2019-04-12 19:53:21 -0400

    WIP: file messsage

--- src/screens/app/MainArea/Chat/redux/saga.js
+++ src/screens/app/MainArea/Chat/redux/saga.js
@@ -2,7 +2,11 @@ import { put, takeLatest, call } from 'redux-saga/effects';
 import notify from '../../../../../utils/notify';
 import * as CONSTANTS from './constants';
 
-import { sendMessageSuccess, sendMessageError } from './actions';
+import {
+  sendMessageSuccess,
+  sendMessageError,
+  sendFileMessageError,
+} from './actions';
 import request from '../../../../../utils/apiRequest';
 
 function* sendMessage(action) {
@@ -24,6 +28,29 @@ function* sendMessage(action) {
   }
 }
 
+function* sendFileMessage(action) {
+  try {
+    const { file, groupId, chatId, text, fileName } = action;
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('text', text);
+    formData.append('filename', fileName);
+
+    yield call(
+      request,
+      `/groups/${groupId}/chats/${chatId}/messages/file`,
+      'POST',
+      formData,
+      true,
+      true,
+    );
+  } catch (err) {
+    notify('error', err.meesage);
+    yield put(sendFileMessageError(err));
+  }
+}
+
 export default function* chatSaga() {
   yield takeLatest(CONSTANTS.SEND_MESSAGE_REQUEST, sendMessage);
+  yield takeLatest(CONSTANTS.SEND_FILE_MESSAGE_REQUEST, sendFileMessage);
 }
