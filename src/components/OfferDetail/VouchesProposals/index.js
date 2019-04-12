019205a560277980dfc45c7f5b99b8ed528e5931
weblegend15 <warrenanderson4036@gmail.com>
2019-04-11 21:03:26 -0500

    update: components

--- src/components/OfferDetail/VouchesProposals/index.js
+++ src/components/OfferDetail/VouchesProposals/index.js
@@ -1,3 +1,14 @@
+import PropTypes from 'prop-types';
 import VouchesProposals from './VouchesProposals';
+
+VouchesProposals.propTypes = {
+  vouches: PropTypes.array,
+  proposals: PropTypes.array,
+};
+
+VouchesProposals.defaultProps = {
+  vouches: [],
+  proposals: [],
+};
 
 export default VouchesProposals;
