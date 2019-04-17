270252d78a379ee5acd98f4594dfa76297f182dd
weblegend15 <warrenanderson4036@gmail.com>
2019-04-17 10:17:33 -0500

    fix(layouts): router link on mobile nav

--- src/layouts/MobileSidebar/index.js
+++ src/layouts/MobileSidebar/index.js
@@ -1,4 +1,14 @@
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import MobileSidebar from './MobileSidebar';
 import './MobileSidebar.scss';
 
-export default MobileSidebar;
+MobileSidebar.propTypes = {
+  selectedGroupId: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = state => ({
+  selectedGroupId: state.app.main.selectedGroupId,
+});
+
+export default connect(mapStateToProps)(MobileSidebar);
