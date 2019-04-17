89834c38e463c89d2034147cb5268db22b32e045
weblegend15 <warrenanderson4036@gmail.com>
2019-04-17 10:04:00 -0500

    feat(screens): enable/disable Two-FA on user settings page

--- src/screens/app/Users/UserSettings/Security/Security.js
+++ src/screens/app/Users/UserSettings/Security/Security.js
@@ -3,20 +3,29 @@ import React, { Component } from 'react';
 import { Toggle } from '../../../../../components';
 
 export default class Security extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isEnabled: false,
-    };
-  }
-
   handleToggle = () => {
-    const { isEnabled } = this.state;
-    this.setState({ isEnabled: !isEnabled });
+    const {
+      toggleModal,
+      generateTwoFARequest,
+      disableTwoFAReqeust,
+      currentUser: { is2faEnabled },
+    } = this.props;
+
+    if (!is2faEnabled) {
+      generateTwoFARequest();
+      toggleModal('addTwoFAModal');
+    }
+
+    if (is2faEnabled) {
+      disableTwoFAReqeust();
+    }
   };
 
   render() {
-    const { isEnabled } = this.state;
+    const {
+      currentUser: { is2faEnabled },
+    } = this.props;
+
     return (
       <div className="p-4 mb-5">
         <div>
@@ -26,10 +35,10 @@ export default class Security extends Component {
           <div className="d-flex flex-row align-items-center">
             <Toggle
               className="mr-4 ml-2"
-              value={isEnabled}
+              value={is2faEnabled}
               onClick={this.handleToggle}
             />
-            <p>{isEnabled ? 'Enabled' : 'Disabled'}</p>
+            <p>{is2faEnabled ? 'Enabled' : 'Disabled'}</p>
           </div>
         </div>
       </div>
--- src/screens/app/Users/UserSettings/Security/index.js
+++ src/screens/app/Users/UserSettings/Security/index.js
@@ -1,3 +1,31 @@
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Security from './Security';
 
-export default Security;
+import toggleModal from '../../../../../modals/redux/actions';
+
+import {
+  generateTwoFARequest,
+  disableTwoFAReqeust,
+} from '../../../../auth/redux/actions';
+
+Security.propTypes = {
+  currentUser: PropTypes.object.isRequired,
+  generateTwoFARequest: PropTypes.func.isRequired,
+  disableTwoFAReqeust: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = state => ({
+  currentUser: state.auth.currentUser,
+});
+
+const mapDispatchToProps = {
+  toggleModal,
+  generateTwoFARequest,
+  disableTwoFAReqeust,
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Security);
